6d459b9e97b95a9ab978c33ea55c1397774d13b1
tanmoy <pdt.tanmoy@gmail.com>
2021-10-27 22:55:48 +0600

    findEmployeeById() method added to get employee by id

--- src/main/java/com/tanmoy/employeeservice/service/EmployeeService.java
+++ src/main/java/com/tanmoy/employeeservice/service/EmployeeService.java
@@ -3,16 +3,21 @@ package com.tanmoy.employeeservice.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.tanmoy.employeeservice.domain.Employee;
 import com.tanmoy.employeeservice.repository.EmployeeRepository;
 
 @Service
 public class EmployeeService {
-	
+
 	private EmployeeRepository empRepo;
-	
+
 	@Autowired
 	public EmployeeService(EmployeeRepository empRepo) {
 		this.empRepo = empRepo;
+	}
+
+	public Employee findEmployeeById(int id) {
+		return empRepo.findById(id);
 	}
 
 }
