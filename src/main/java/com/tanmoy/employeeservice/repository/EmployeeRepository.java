f5488210aebb663538eb44a892d5a8cac00c4596
tanmoy <pdt.tanmoy@gmail.com>
2021-10-27 22:53:22 +0600

    Added findById() to get employee by id

--- src/main/java/com/tanmoy/employeeservice/repository/EmployeeRepository.java
+++ src/main/java/com/tanmoy/employeeservice/repository/EmployeeRepository.java
@@ -5,5 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.tanmoy.employeeservice.domain.Employee;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+	
+	Employee findById(int id);
 
 }
