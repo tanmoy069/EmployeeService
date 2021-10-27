8ea874d0fbf5e47e463ceff41bece47fe508033d
tanmoy <pdt.tanmoy@gmail.com>
2021-10-27 22:48:43 +0600

    EmployeeRepository has been created

---
+++ src/main/java/com/tanmoy/employeeservice/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.tanmoy.employeeservice.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.tanmoy.employeeservice.domain.Employee;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+}
