cdafb05f389417eac1a9c3a74b4f9919eba9488b
tanmoy <pdt.tanmoy@gmail.com>
2021-10-27 22:50:52 +0600

    EmployeeService has been created

---
+++ src/main/java/com/tanmoy/employeeservice/service/EmployeeService.java
@@ -0,0 +1,18 @@
+package com.tanmoy.employeeservice.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.tanmoy.employeeservice.repository.EmployeeRepository;
+
+@Service
+public class EmployeeService {
+	
+	private EmployeeRepository empRepo;
+	
+	@Autowired
+	public EmployeeService(EmployeeRepository empRepo) {
+		this.empRepo = empRepo;
+	}
+
+}
