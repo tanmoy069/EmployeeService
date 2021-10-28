989039ee3bae014bc846a7e9c9812aa14aa047a4
tanmoy <pdt.tanmoy@gmail.com>
2021-10-28 22:57:56 +0600

    /employee/findby? id or code get rest api created to get employee by id or code

--- src/main/java/com/tanmoy/employeeservice/controller/EmployeeController.java
+++ src/main/java/com/tanmoy/employeeservice/controller/EmployeeController.java
@@ -1,20 +1,31 @@
 package com.tanmoy.employeeservice.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.tanmoy.employeeservice.domain.Employee;
 import com.tanmoy.employeeservice.service.EmployeeService;
 
 @RestController
 @RequestMapping("/employee")
 public class EmployeeController {
-	
+
 	private EmployeeService empService;
-	
+
 	@Autowired
 	public EmployeeController(EmployeeService empService) {
 		this.empService = empService;
+	}
+
+	@GetMapping("/findby")
+	public Employee findEmployeeBy(@RequestParam(name = "id", required = false) Integer id,
+			@RequestParam(name = "code", required = false) String code) {
+		if (id != null)	return empService.findEmployeeById(id);
+		if (code != null) return empService.findEmployeeByCode(code);
+		return null;
 	}
 
 }
