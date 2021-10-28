e513a30b6dfa80c33da6032ddf2fc252f3a9b4e4
tanmoy <pdt.tanmoy@gmail.com>
2021-10-28 23:07:11 +0600

    /employee/save post rest api added to save employee

--- src/main/java/com/tanmoy/employeeservice/controller/EmployeeController.java
+++ src/main/java/com/tanmoy/employeeservice/controller/EmployeeController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,6 +35,11 @@ public class EmployeeController {
 	@GetMapping("/findall")
 	public List<Employee> findAllEmployee() {
 		return empService.findAll();
+	}
+	
+	@PostMapping("/save")
+	public boolean saveEmployee(@RequestBody Employee employee) {
+		return empService.saveEmployee(employee);
 	}
 
 }
